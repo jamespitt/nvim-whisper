17896c5b0a2bd6c484352819a742a8db2adf257b
James Pitt <james.pitt@gmail.com>
2024-03-04 14:51:39 +0000

    test to make sure code is running

--- lua/nvim-whisper/module.lua
+++ lua/nvim-whisper/module.lua
@@ -3,7 +3,9 @@ local M = {}
 
 ---@return string
 M.my_first_function = function(greeting)
+  vim.api.nvim_echo({{msg = "Variable value: " .. variable_name, timeout = 1000}})`
   return greeting
 end
+
 
 return M
