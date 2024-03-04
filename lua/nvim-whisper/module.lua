458dddb041961f110d43cac740d6413477d367b2
James Pitt <james.pitt@gmail.com>
2024-03-04 14:53:50 +0000

    fix typo

--- lua/nvim-whisper/module.lua
+++ lua/nvim-whisper/module.lua
@@ -3,7 +3,7 @@ local M = {}
 
 ---@return string
 M.my_first_function = function(greeting)
-  vim.api.nvim_echo({{msg = "Variable value: " .. variable_name, timeout = 1000}})`
+  vim.api.nvim_echo({{msg = "Variable value: " .. greeting, timeout = 1000}})
   return greeting
 end
 
